2bea791a8705bd4d3c71f5919ff315eeb96fde16
jmb11x <99753424+jmb11x@users.noreply.github.com>
2024-03-30 15:37:55 -0400

    finish first version of drone-project page. Going to attempt to consolidate entire repo into pages

---
+++ scripts/drone-project.js
@@ -0,0 +1,6 @@
+
+// rgb to grayscale bounce
+setInterval(function() {
+    var imageContainer = document.getElementById("rgb-cluster");
+    imageContainer.classList.toggle('gray-scale');
+  }, 1000);
